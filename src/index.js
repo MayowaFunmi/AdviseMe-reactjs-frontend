b947285db5ff017ba0b8d5bf00cfa188e1b8aa68
Akinade Mayowa <akinade.mayowa@gmail.com>
2021-05-10 01:01:13 +0100

    added login page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 
 ReactDOM.render(
   <React.StrictMode>
